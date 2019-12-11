9919feebe2f7e308b210d6c21049faefcfad5348
jaywonger <48599319+jaywonger@users.noreply.github.com>
2019-12-11 11:23:41 -0600

    Added Phase 8 coverage groups

---
+++ verification/phase8_coverage/driver.sv
@@ -0,0 +1,42 @@
+`ifndef DRIVER_SV
+`define DRIVER_SV
+
+`include "verification/phase8_coverage/transaction.sv"
+
+class driver;
+  //used to count the number of transactions
+  int no_transactions;
+
+  //creating virtual interface handle
+  virtual downstream vif;
+
+  //creating mailbox handle
+  mailbox gen2driv, driv2scb;
+
+  //constructor
+  function new(virtual downstream vif, mailbox gen2driv, mailbox driv2scb);
+    //getting the interface
+    this.vif = vif;
+    //getting the mailbox handles from environment
+    this.gen2driv = gen2driv;
+    this.driv2scb = driv2scb;
+  endfunction
+
+  //drive the transaction items to interface signals
+  task main();
+    transaction trans;
+    forever begin
+      @vif.cb_down;
+        gen2driv.get(trans);
+        vif.cb_down.valid_in <= 1'b1;
+        vif.cb_down.data_in <= trans.data_in;
+        vif.cb_down.addr_in <= trans.addr_in;
+        trans.display("[ Driver ]");
+        driv2scb.put(trans);
+        no_transactions++;
+    end
+  endtask
+
+endclass
+
+`endif
---
+++ verification/phase8_coverage/generator.sv
@@ -0,0 +1,48 @@
+`ifndef GENERATOR_SV
+`define GENERATOR_SV
+
+`include "verification/phase8_coverage/transaction.sv"
+`include "verification/phase8_coverage/coverage.sv"
+
+class generator;
+  //declaring transaction class
+  rand transaction trans, driv_trans;
+  coverage cov;
+
+  // count number of transactions
+  int no_transactions;
+
+  //repeat count, to specify number of items to generate
+  int repeat_count;
+
+  // mailboxes
+  mailbox gen2driv[4];
+
+  //constructor
+  function new(mailbox gen2driv[4]);
+    this.gen2driv[0] = gen2driv[0];
+    this.gen2driv[1] = gen2driv[1];
+    this.gen2driv[2] = gen2driv[2];
+    this.gen2driv[3] = gen2driv[3];
+    cov = new();
+  endfunction
+
+  //main task, generates (creates and randomizes) transaction packets
+  task main();
+    trans = new();
+    repeat(repeat_count) begin
+      if( !trans.randomize() ) $fatal("Gen:: trans randomization failed");
+      no_transactions++;
+      driv_trans = new();
+      driv_trans.data_in = trans.data_in;
+      driv_trans.addr_in = trans.addr_in;
+      driv_trans.port_in = trans.port_in;
+      trans.display("[ Generator ]");
+      gen2driv[driv_trans.port_in].put(driv_trans);
+      cov.sample_trans(driv_trans);
+    end
+  endtask
+
+endclass
+
+`endif
